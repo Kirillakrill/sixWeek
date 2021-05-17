7ac1340d7c1adb1784057ca23bd7f75a205cc0d1
Kirillakrill <>
2021-05-17 14:45:37 +0000

    Add route "insert"

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-export default (express, bodyParser, fs, crypto, CORS, http) => {
+export default (express, bodyParser, createReadStream, writeFileSync, crypto, http, m, User) => {
     const app = express();
+    const urlencodedParser = bodyParser.urlencoded({ extended: false });
+    const json_parser = bodyParser.json();
+    const url = 'mongodb+srv://reader:Vpsral9003@cluster0.rabsn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
     app
-    .use((r, res, next) => r.res.set(CORS) && next())
+    .use((r, res, next) => r.res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE"}) && next())
     .use(bodyParser.urlencoded({ extended: true }))
     .all('/req/', (req, res) => {
         const addr = req.method === 'POST' ? req.body.addr : req.query.addr;
@@ -13,9 +18,25 @@ export default (express, bodyParser, fs, crypto, CORS, http) => {
             .on('end', () => res.send(b));
         });
     })
-    .get('/login/', (req, res) => res.send('akrillsm9003'))   
+    .get('/login/', (req, res) => res.send('akrillsm9003'))
     .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
     .get('/sha1/:input/', (req, res) => res.send(crypto.createHash('sha1').update(req.params.input).digest('hex')))
+    .post('/insert/', urlencodedParser, async (req, res) => {
+        const log = req.body.login;
+        const pass = req.body.password;
+        const url = req.body.URL;
+
+        await m.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        const newUser = new User({login: log, password: pass});
+        try {
+            await newUser.save();
+            res.status(201);
+        } catch (e) {
+            res.status(400);
+        }
+        res.end();
+    })
     .all('/*', r => r.res.send('akrillsm9003'));
 
     return app;
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import fs from 'fs';
-import CORS from './CORS.js';
+import { createReadStream, writeFileSync } from 'fs';
 import crypto from 'crypto';
 import http from 'http';
-
 import appSrc from './app.js';
-const app = appSrc(express, bodyParser, fs, crypto, CORS, http);
-app.listen(process.env.PORT);
+import m from 'mongoose'
+import UserModel from './User.js';
+
+const User = UserModel(m);
+const app = appSrc(express, bodyParser, createReadStream, writeFileSync, crypto, http, m, User);
+try {
+    app.listen(process.env.PORT || 4321);
+} catch(e) {
+    console.log(e.codeName);
+}
